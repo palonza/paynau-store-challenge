502229fe9912bf7954a11ffeacd6ea4877438842
palonza <palonza@gmail.com>
2025-10-06 00:18:40 -0300

    init commit - done

--- frontend/src/components/common/PageHeader.tsx
+++ frontend/src/components/common/PageHeader.tsx
@@ -1,10 +1,14 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+// import React from "react";
+import { AppBar, Toolbar, Typography, Button, IconButton, Tooltip } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
 import { Link, useLocation } from "react-router-dom";
+import { useThemeMode } from "../../theme/ThemeContext";
 
 export default function PageHeader() {
   const location = useLocation();
+  const { mode, toggleMode } = useThemeMode();
 
   // Determina el título dinámicamente según la ruta
   const getTitle = () => {
@@ -26,14 +30,24 @@ export default function PageHeader() {
           {getTitle()}
         </Typography>
 
-        <Button
-          component={Link}
-          to="/"
-          color="inherit"
-          startIcon={<HomeIcon />}
-        >
-          Inicio
-        </Button>
+        <div>
+            <Tooltip title="Ir al inicio">
+                <Button
+                component={Link}
+                to="/"
+                color="inherit"
+                startIcon={<HomeIcon />}
+                >
+                Inicio
+                </Button>
+            </Tooltip>
+
+            <Tooltip title={`Cambiar a modo ${mode === "light" ? "oscuro" : "claro"}`}>
+                <IconButton color="inherit" onClick={toggleMode}>
+                    {mode === "light" ? <Brightness4Icon /> : <Brightness7Icon />}
+                </IconButton>
+            </Tooltip>
+        </div>
       </Toolbar>
     </AppBar>
   );
